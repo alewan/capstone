996cd91377ac5e1051c7d1445126463a75e9dfdf
Aleksei Wan <aleksei.wan@mail.utoronto.ca>
2019-10-27 15:09:01 -0400

    MISC Make Environment YAML usable from multiple platforms
    -Change the environment export so that it no longer contains build info.
      This will allow the environmental YAML to be used from different OSes.
    -Add a script to remove macOS specific packages and invoke it by default.
    -Leave an alias to get full env export in case it is needed later.
    
    Issue-Id: #10
    ENV-UP

--- aliases.sh
+++ aliases.sh
@@ -1,4 +1,13 @@
 # Source this file to access the aliases
 
-# Helper aliases
-alias export_env="conda env export > ~/capstone/envs/environment.yaml"
+# Set constants and print them
+ENV_DIR="~/capstone/envs"
+echo "Using $ENV_DIR as environments directory"
+
+# Environment Management aliases
+# Update an existing environment
+alias update_env="conda env update --file $ENV_DIR/environment.yaml  --prune"
+# Export environment file for committing
+alias export_env="conda env export --no-builds > $ENV_DIR/environment.yaml &&  $ENV_DIR/process_environment.sh"
+# Export the complete environment information
+alias export_full_env="conda env export > $ENV_DIR/full_environment.yaml"
