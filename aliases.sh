0dc21a292bcf34308439158c28ed0290a12c0049
Aleksei Wan <aleksei.wan@mail.utoronto.ca>
2019-11-25 23:18:19 -0500

    NN VIS Add LightGBM for Final Emotion Guesses
    - Add LightGBM to provide our final guess.
    - Implemented its usage on AWS image data.
    - The end goal would be to add audio data to the LGBM model to further
    improve it.
    - Add jpg_dump_results, jpg_dump_2 JSONs to cached_results.
    - Edit aliases file to remove pruning when updating the environment
    because it can make it difficult when changing the environment file.

--- aliases.sh
+++ aliases.sh
@@ -6,7 +6,7 @@ echo "Using $ENV_DIR as environments directory"
 
 # Environment Management aliases
 # Update an existing environment
-alias update_env="conda env update --file $ENV_DIR/environment.yaml  --prune"
+alias update_env="conda env update --file $ENV_DIR/environment.yaml"
 # Export environment file for committing
 alias export_env="conda env export --no-builds > $ENV_DIR/environment.yaml &&  $ENV_DIR/process_environment.sh"
 # Export the complete environment information
--- scripts/process_ravdess_naming.py
+++ scripts/process_ravdess_naming.py
@@ -6,10 +6,13 @@ import re
 
 RAVDESS_STRING = r'(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-(\d+)-(\d+)'
 RAVDESS_NAME = re.compile(RAVDESS_STRING)
-RAVDESS_FILE = re.compile(RAVDESS_STRING+'.mp4$')
+RAVDESS_FILE = re.compile(RAVDESS_STRING + '.mp4$')
 
 RAVDESS_EMOTION_MAPPING = ['NEUTRAL', 'CALM', 'HAPPY', 'SAD', 'ANGRY', 'FEARFUL', 'DISGUST', 'SURPRISED']
-AWS_RAVDESS_LIST = ['NEUTRAL', 'CALM', 'HAPPY', 'SAD', 'ANGRY', 'FEARFUL', 'DISGUSTED', 'SURPRISED']
+AWS_RAVDESS_LIST = ['NEUTRAL', 'CALM', 'HAPPY', 'SAD', 'ANGRY', 'FEAR', 'DISGUSTED', 'SURPRISED']
+AWS_EMOTION_DICT = {'NEUTRAL': 0, 'CALM': 1, 'HAPPY': 2, 'SAD': 3, 'ANGRY': 4, 'FEAR': 5, 'DISGUSTED': 6,
+                    'SURPRISED': 7, 'CONFUSED': 8}
+
 
 def is_ravdess_file(file_name: str) -> bool:
     return re.match(RAVDESS_FILE, file_name) is not None
@@ -34,3 +37,18 @@ def get_emotion_from_ravdess_name_aws(name: str) -> str:
         idx = int(a.group(3)) - 1
         return AWS_RAVDESS_LIST[idx] if idx < len(AWS_RAVDESS_LIST) else 'ERROR - ' + a.group(3) + 'out of range'
     return 'ERROR - No match found'
+
+
+def get_emotion_num_from_ravdess_name(name: str) -> int:
+    a = re.match(RAVDESS_NAME, name)
+    if a is not None:
+        idx = int(a.group(3)) - 1
+        return idx if idx < len(RAVDESS_EMOTION_MAPPING) else -1
+    return -1
+
+
+def aws_number_from_emotion(emotion: str) -> int:
+    try:
+        return AWS_EMOTION_DICT[emotion]
+    except KeyError:
+        return -1
