c417bea20201f3cf3665e516972f8e7366c3cbfb
Olivia Roscoe <olivia.roscoe@mail.utoronto.ca>
2020-02-17 12:36:26 -0500

    AUD NN Create standalone function for pipeline
    - created standalone function to use a pretrained nn to classify data

--- audio_nn/use_nn.py
+++ audio_nn/use_nn.py
@@ -8,6 +8,25 @@ import audio_nn as nn
 from argparse import ArgumentParser
 from numpy import savetxt
 
+
+def use_nn(audio_dir: str, checkpoint: str, batch_size: int):
+    """
+       Get an audio prediction from a pretrained network to csv
+       :param aud_dir: path to folder containing audio spectral imgs
+       :param checkpoint: path to audio nn checkpoint file to load model from
+       :param batch_size: batch size to process data in
+    """
+    model = nn.AudioNN()
+    model.load_from_checkpoint(checkpoint)
+
+    dataloader = nn.load_data_with_filename(audio_dir, batch_size, model)
+    pred = model.get_prediction(dataloader)[0]
+
+    # do something with pred, for now just save to CSV
+    savetxt('audio_nn_predictions.csv', pred[0].numpy(), delimiter=",", fmt='%s')
+    savetxt('audio_nn_prediction_names.csv', pred[1], delimiter=",", fmt='%s')
+
+
 if __name__ == "__main__":
     parser = ArgumentParser(description='Get predictions from audio neural net')
     parser.add_argument('--audio_dir', '-a', type=str, help='Dir containing audio spectograms for classification',
@@ -27,12 +46,4 @@ if __name__ == "__main__":
         print('Provided path', args.audio_dir, 'is not a valid directory. Please try again')
         exit(-1)
 
-    model = nn.AudioNN()
-    model.load_from_checkpoint(args.checkpoint)
-
-    dataloader = nn.load_data_with_filename(args.audio_dir, args.batch_size, model)
-    pred = model.get_prediction(dataloader)[0]
-
-    # do something with pred, for now just save to CSV
-    savetxt('audio_nn_predictions.csv', pred[0].numpy(), delimiter=",", fmt='%s')
-    savetxt('audio_nn_prediction_names.csv', pred[1], delimiter=",", fmt='%s')
+    use_nn(args.audio_dir, args.checkpoint, args.batch_size)
