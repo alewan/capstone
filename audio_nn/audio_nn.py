fa8cd303a4bbd52daeec415ef196fd4f78b6b005
Aleksei Wan <aleksei.wan@pm.me>
2020-03-31 10:27:40 -0400

    WIP

--- audio_nn/audio_nn.py
+++ audio_nn/audio_nn.py
@@ -12,17 +12,17 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.utils.data as utils
-from torchvision import transforms
+from torchvision import transforms, models
 
 # TODO: Look for better way to deal with hardcoded filepath
 path.insert(1, os.path.join(path[0], '../scripts'))
-from process_naming import get_emotion_num_from_ravdess_name
+import process_naming as pn
 
 from datetime import datetime
 import numpy as np
 
 # regex for image file matching
-IMG_FILE = re.compile('(.*)rgb_plt\.png$')
+IMG_FILE = re.compile('(.*)rgb_plt(.*).png$')
 
 
 # 2 layer linear fully connected neural network
@@ -33,31 +33,26 @@ class AudioClassifier(nn.Module):
         super(AudioClassifier, self).__init__()
         self.name = "audio"
 
-        # Conv2d(in_channel, out_channel, kernel_size, stride=1, padding=0)
-        # output_size = (in_size - k_size + 2*padding)/stride + 1
-        self.conv1 = nn.Conv2d(4, 6, 5)
-        self.conv2 = nn.Conv2d(6, 10, 5)
+        self.fc1 = nn.Linear(256 * 5 * 3, 1050)
+        self.fc2 = nn.Linear(1050, 380)
+        self.fc3 = nn.Linear(380, 8)
 
-        # This is a generic max pooling layer MaxPool2d(kernel_size, stride)
-        # output_size = (in_size - kernel_size)/stride + 1
-        self.pool = nn.MaxPool2d(2, 2)
-
-        self.fc1 = nn.Linear(10 * 47 * 32, 50)
-        self.fc2 = nn.Linear(50, 8)
+        self.drop = nn.Dropout(p=0.15)
 
     # inputs to the nn must be tensors of form [N, C, H, W]
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))  # output: 6x98x68
-        x = self.pool(F.relu(self.conv2(x)))  # output: 10x47x32
-        x = x.view(-1, 10 * 47 * 32)
+        x = x.view(-1, 256 * 5 * 3)
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = F.relu(self.fc2(self.drop(x)))
+        x = self.fc3(self.drop(x))
         return x
 
 
 def generate_data_label(filename: str) -> torch.tensor:
-    # TODO: will need to generalize once more datasets are in use
-    emotion = get_emotion_num_from_ravdess_name(filename)
+    if pn.is_ravdess_name(filename):
+        emotion = pn.get_emotion_num_from_ravdess_name(filename)
+    elif pn.is_crema_name(filename):
+        emotion = pn.get_emotion_num_from_crema_name(filename)
     return torch.tensor(emotion)
 
 
@@ -76,7 +71,7 @@ def load_data(directory, batch_size):
             # Transforms: i) Add padding to horizontal dimension of image
             #            ii) crop to be 140x200
             #           iii) convert image to tensor
-            image_list.append(data_transform(Image.open(os.path.join(directory, filename))))
+            image_list.append(data_transform(Image.open(os.path.join(directory, filename)).convert('RGB')))
             labels.append(generate_data_label(filename))
         else:
             print('Ignoring non-image file', filename)
@@ -126,20 +121,63 @@ class AudioNN:
         if not os.path.exists(self.checkpoint_dir):
             os.mkdir(self.checkpoint_dir)
         self.model = AudioClassifier()
-        self.optimizer = optim.SGD(self.model.parameters(), lr=0.01, momentum=0.9)
         self.name_list = []
+        # self.use_cuda = torch.cuda.is_available()
+        # if self.use_cuda:
+        #     print("Using cuda")
+        #     self.model.cuda()
+        self.optimizer = optim.Adam(self.model.parameters(), lr=0.0001)
+        self.alexnet = models.alexnet(pretrained=True)
+
+    def load_data(self, directory, batch_size):
+        image_list = []
+        labels = []
+        # padding is the colour of silence in the clips
+        data_transform = transforms.Compose([transforms.Pad((60, 0), fill=(0, 127, 127)),
+                                             transforms.CenterCrop((200, 140)),
+                                             transforms.ToTensor()])
+
+        print('Using', directory, 'as audio directory... ')
+
+        for filename in os.listdir(directory):  # assuming jpg
+            img_file = re.match(IMG_FILE, filename)
+            if img_file:
+                img_path = os.path.join(directory, filename)
+                im = Image.open(img_path).convert('RGB')
+                # Transforms: i) Add padding to horizontal dimension of image
+                #            ii) crop to be 140x200
+                #           iii) convert image to tensor
+                im = data_transform(im)
+                image_list.append(im)
+
+                # TODO: will need to generalize once more datasets are in use
+                emotion = get_emotion_num_from_ravdess_name(filename)
+                labels.append(torch.tensor(emotion))
+            else:
+                print('Ignoring non-image file ', filename)
+
+        tensor_img = torch.stack(image_list)
+        tensor_labels = torch.stack(labels)
+
+        my_dataset = utils.TensorDataset(tensor_img, tensor_labels)  # create your datset
+        my_dataloader = utils.DataLoader(my_dataset, batch_size=batch_size, shuffle=True)
+
+        return my_dataloader
 
     def train_network(self, train, batch_size, num_epochs, valid):
         criterion = nn.CrossEntropyLoss()
-        iters, losses, train_acc, val_acc = [], [], [], []
-
+        train_acc, val_acc = np.zeros(num_epochs), np.zeros(num_epochs)
         checkpoint_datetime = datetime.now().strftime("%Y%m%d-%H%M%S")
 
         # training
-        n = 0  # the number of iterations
         for epoch in range(num_epochs):
             print("Training epoch", epoch)
             for imgs, labels in train:
+                imgs = self.alexnet.features(imgs)
+                # if self.use_cuda:
+                #     imgs = imgs.cuda()
+                #     labels = labels.cuda()
+
                 self.model.train()
                 out = self.model(imgs)  # forward pass
                 loss = criterion(out, labels)  # compute the total loss
@@ -147,23 +185,24 @@ class AudioNN:
                 self.optimizer.step()  # make the updates for each parameter
                 self.optimizer.zero_grad()  # a clean up step for PyTorch
 
-                # save the current training information every iteration
-                iters.append(n)
-                losses.append(float(loss) / batch_size)  # compute *average* loss
-                train_acc.append(self.get_accuracy(train))  # compute training accuracy
-                if valid is not None:
-                    val_acc.append(self.get_accuracy(valid))  # compute validation accuracy
-                n += 1
+            # save the current training information every iteration
+            #losses.append(float(loss) / batch_size)  # compute *average* loss
+            train_acc[epoch] = self.get_accuracy(train)  # compute training accuracy
+            if valid is not None:
+                val_acc[epoch] = self.get_accuracy(valid)  # compute validation accuracy
 
             # save the current model parameters
-            model_info = checkpoint_datetime + "_{0}_bs{1}_epoch{2}".format(self.model_name, batch_size, epoch)
+            device = "cpu"
+            # if self.use_cuda:
+            #     device = "gpu"
+
+            model_info = checkpoint_datetime + "_{0}_bs{1}_epoch{2}_{3}".format(self.model_name, batch_size, epoch, device)
             checkpoint_path = os.path.join(self.checkpoint_dir, model_info)
             self.checkpoint_model(epoch, loss, checkpoint_path)
 
-            np.savetxt(os.path.join(self.checkpoint_dir, checkpoint_datetime + "_acc.csv"), train_acc, delimiter=",",
-                       fmt='%s')
-            np.savetxt(os.path.join(self.checkpoint_dir, checkpoint_datetime + "_loss.csv"), losses, delimiter=",",
-                       fmt='%s')
+            np.savetxt(os.path.join(self.checkpoint_dir, checkpoint_datetime + "_acc.csv"), train_acc, delimiter=",", fmt='%s')
+            #np.savetxt(os.path.join(self.checkpoint_dir, checkpoint_datetime + "_loss.csv"), losses, delimiter=",",
+            #           fmt='%s')
 
             if valid is not None:
                 np.savetxt(os.path.join(self.checkpoint_dir, checkpoint_datetime + "_val_acc.csv"), val_acc,
@@ -176,6 +215,10 @@ class AudioNN:
         total = 0
         self.model.eval()
         for imgs, labels in data:
+            imgs = self.alexnet.features(imgs)
+            # if self.use_cuda:
+            #     imgs = imgs.cuda()
+            #     labels = labels.cuda()
             output = self.model(imgs)
 
             # select index with maximum prediction score
@@ -208,6 +251,10 @@ class AudioNN:
         self.model.eval()
         predictions = []
         for imgs, name_ids in data:
+            imgs = self.alexnet.features(imgs)
+            # if self.use_cuda:
+                # imgs = imgs.cuda()
+
             output = self.model(imgs)
             predictions.append((output.data, [self.name_list[i] for i in range(len(imgs))]))
         return predictions
